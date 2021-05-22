2487967a8caa697f3b1889718555fe76cec6e105
Shwetanka Singh <shwetanka99.dipa@gmail.com>
2021-05-22 10:50:13 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
+import { Todo } from './todo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstApp';
+  title = 'Welcome to our new App';
+  
+    todoValue:string;
+    list:Todo[];
+    constructor(){
+      this.todoValue="";
+      this.list=[];
+    }
+  
+  ngOnInit()
+  {
+    this.list=[];
+    this.todoValue="";
+  }
+
+  addItem()
+  {
+     if(this.todoValue!="")
+     {
+       const newItem: Todo ={
+         id:Date.now(),
+         value:this.todoValue,
+        isDone:false
+       };
+       this.list.push(newItem);
+     }
+     this.todoValue="";
+  }
+
+  deleteItem(id:number){
+   this.list=this.list.filter(item=>item.id!=id);
+  }
 }
